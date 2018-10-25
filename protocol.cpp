a467ba466f412ff121012d155424e3130c80c9af
Peter Bushnell <bushsolo@gmail.com>
2018-10-25 08:20:24 +0100

    BitcoinPlus

--- protocol.cpp
+++ protocol.cpp
@@ -22,7 +22,7 @@ static const char* ppszTypeName[] =
     "block",
 };
 
-unsigned char pchMessageStart[4] = { 0xf9, 0xbe, 0xb4, 0xd9 };
+unsigned char pchMessageStart[4] = { 0x2d, 0x3f, 0xa2, 0xf5 };
 
 CMessageHeader::CMessageHeader()
 {
